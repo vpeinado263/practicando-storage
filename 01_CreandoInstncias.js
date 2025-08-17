c4aaa23f25788c2c3f70ce00a67b05afa09a4c00
Victor Peinado <vpeinado263@gmail.com>
2025-08-17 19:13:16 -0300

    Finalizar eventos

---
+++ 01_CreandoInstncias.js
@@ -0,0 +1,168 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Evento para guardar formulario pelicula
+let formulario = document.getElementById("formulario");
+formulario.addEventListener("submit", guardarPelicula);
+
+// Evento para dark mode.
+// Obtiene el id del botón desde el html y le asigna el evento click
+let botonDarkMode = document.getElementById("darkMode");
+botonDarkMode.addEventListener("click", cambiarTema);
+
+// Funcion asociada al evento para cambiar tema a darkMode
+// (chequear "darkMode" en el css)
+// En este caso está aplicada directamente sobre el body
+function cambiarTema() {
+    document.body.classList.toggle("darkMode");
+
+    if (document.body.classList == "darkMode") {
+        localStorage.setItem("darkMode", true);
+    } else {
+        localStorage.setItem("darkMode", false);
+    }
+}
+
+function chequearDarkMode() {
+    const darkMode = localStorage.getItem("darkMode");
+    console.log(darkMode);
+
+    if (darkMode = "true") {
+        document.body.classList = "darkMode";
+    }
+}
+
+chequearDarkMode();
